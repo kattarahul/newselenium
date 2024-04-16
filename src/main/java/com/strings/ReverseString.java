3d94d4d255f983fe3393e639004a0ce579e86e4d
kattarahul <kattarahul72@gmail.com>
2024-04-17 01:19:36 +0530

    new update.

--- src/main/java/com/strings/ReverseString.java
+++ src/main/java/com/strings/ReverseString.java
@@ -1,2 +1,26 @@
-package com.strings;public class ReverseString {
+package com.strings;
+
+public class ReverseString {
+
+
+    public static void main(String[] args)
+    {
+        String str = "Testing";
+
+        String str2 ="";
+
+
+        char ch = 0;
+
+        for(int i = str.length()-1;i>=0; i--)
+        {
+            str2 = str2 + str.charAt(i);
+
+        }
+
+        System.out.println(str2);
+
+    }
+
+
 }
